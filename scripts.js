5dd338825bdc9b3269cef29f9c559fa728347270
Dexter Ty <dexter.gets@gmail.com>
2023-11-10 00:00:40 -0500

    add links top of page

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 const apiKey = process.env.NEWS_API_KEY;
-const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+const bbcNewsUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`;
+const businessNewsUrl = `https://newsapi.org/v2/top-headlines?category=business&apiKey=${apiKey}`;
+const sportsNewsUrl = `https://newsapi.org/v2/top-headlines?category=sports&apiKey=${apiKey}`;
+const technologyNewsUrl = `https://newsapi.org/v2/top-headlines?category=technology&apiKey=${apiKey}`;
+const scienceNewsUrl = `https://newsapi.org/v2/top-headlines?category=science&apiKey=${apiKey}`;
 
 
 async function fetchNews() {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        //console.log(data);
+        console.log(data);
         displayNews(data.articles);
         return data.articles;
     }
@@ -65,6 +70,115 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('searchButton').addEventListener('click', searchNews);
 });
 
+async function fetchBbcNews() {
+    try {
+        document.getElementById('news').innerHTML = '';
+        const response = await fetch(bbcNewsUrl);
+        const data = await response.json();
+        console.log(data);
+        displayNews(data.articles);
+        return data.articles;
+    }
+    catch(error) {
+        console.error("There was an error", error);
+        return [];
+    }
+}
 
+
+document.getElementById('news').addEventListener('click', function(event) {
+    // Prevent the default action of the anchor tag
+    event.preventDefault();
+     // Call your function here
+    fetchBbcNews();
+});
+
+async function fetchBusinessNews() {
+    try {
+        document.getElementById('news').innerHTML = '';
+        const response = await fetch(businessNewsUrl);
+        const data = await response.json();
+        console.log(data);
+        displayNews(data.articles);
+        return data.articles;
+    }
+    catch(error) {
+        console.error("There was an error", error);
+        return [];
+    }
+}
+
+document.getElementById('businessnews').addEventListener('click', function(event) {
+    // Prevent the default action of the anchor tag
+    event.preventDefault();
+     // Call your function here
+    fetchBusinessNews();
+});
+
+async function fetchSportsNews() {
+    try {
+        document.getElementById('news').innerHTML = '';
+        const response = await fetch(sportsNewsUrl);
+        const data = await response.json();
+        console.log(data);
+        displayNews(data.articles);
+        return data.articles;
+    }
+    catch(error) {
+        console.error("There was an error", error);
+        return [];
+    }
+}
+
+document.getElementById('sportsnews').addEventListener('click', function(event) {
+    // Prevent the default action of the anchor tag
+    event.preventDefault();
+     // Call your function here
+    fetchSportsNews();
+});
+
+async function fetchScienceNews() {
+    try {
+        document.getElementById('news').innerHTML = '';
+        const response = await fetch(scienceNewsUrl);
+        const data = await response.json();
+        console.log(data);
+        displayNews(data.articles);
+        return data.articles;
+    }
+    catch(error) {
+        console.error("There was an error", error);
+        return [];
+    }
+}
+
+document.getElementById('sciencenews').addEventListener('click', function(event) {
+    // Prevent the default action of the anchor tag
+    event.preventDefault();
+     // Call your function here
+    fetchScienceNews();
+});
+
+async function fetchTechnologyNews() {
+    try {
+        document.getElementById('news').innerHTML = '';
+        const response = await fetch(technologyNewsUrl);
+        const data = await response.json();
+        console.log(data);
+        displayNews(data.articles);
+        return data.articles;
+    }
+    catch(error) {
+        console.error("There was an error", error);
+        return [];
+    }
+}
+
+document.getElementById('technologynews').addEventListener('click', function(event) {
+    // Prevent the default action of the anchor tag
+    event.preventDefault();
+     // Call your function here
+    fetchTechnologyNews();
+});
 
 fetchNews();
